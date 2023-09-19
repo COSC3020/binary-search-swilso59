a81cc8b9b1732fb932fe4e905c00522cfd743176
Sean W <104464288+swilso59@users.noreply.github.com>
2023-09-19 15:28:16 -0600

    Update code.js
    I believe this is correct although can not get a result to output.

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
-function binarySearch(list, element) {
+function binarySearch(list, element) {  
+    list.sort();
+    let l= 0;
+    let k = list.length-1;
+while (l <= k){
+        var mid = Math.floor((k+l)/2);
+        if (list[mid] === element){
+            return mid;
+        }
+        else if (list[mid] < element){
+            k = mid+1;
+        }
+        else{ 
+            l = mid-1;
+        }
+    }
     return -1;
 }
+
+console.log(binarySearch([1,2,3,4,5]),3);
+//For some reason I am not getting any output when trying to print out the results of the list and element form above. 
