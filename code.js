a0c25df27bbabf97e79313aa087fda77b1e82ea9
Sean W <104464288+swilso59@users.noreply.github.com>
2023-12-06 18:26:19 -0700

    Update code.js
    Corrected parentheses and call.

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function binarySearch(list, element) {  
-    list.sort();
+
     let l= 0;
     let k = list.length-1;
-while (l <= k){
-        var mid = Math.floor((k+l)/2);
+    while (l <= k){
+        var mid = Math.round((k+l)/2);
         if (list[mid] === element){
             return mid;
         }
@@ -17,5 +17,5 @@ while (l <= k){
     return -1;
 }
 
-console.log(binarySearch([1,2,3,4,5],3));
+console.log(binarySearch([2,4,3,5,4,3,2,6,],3));
 // Fixed the parentheses. 
