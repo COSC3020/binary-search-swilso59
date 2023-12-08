7a2009698ef644aabab3a969f2f81d63f8ee94af
Sean W <104464288+swilso59@users.noreply.github.com>
2023-12-08 11:12:40 -0700

    Update code.js
    Should be fixed.

--- code.js
+++ code.js
@@ -2,20 +2,29 @@ function binarySearch(list, element) {
 
     let l= 0;
     let k = list.length-1;
+    if(list.length<1) return -1;
+    //if (list.length === 0){
+        //return false;
+   // }
     while (l <= k){
-        var mid = Math.round((k+l)/2);
+        
+        var mid = Math.floor((k+l)/2);
+    
         if (list[mid] === element){
+            while (mid > 0 && list[mid - 1] === element) {
+                mid--;
+            }
             return mid;
         }
         else if (list[mid] < element){
-            k = mid+1;
+            l = mid+1;
         }
         else{ 
-            l = mid-1;
+            k = mid-1;
         }
     }
-    return -1;
+    return false;
 }
 
-console.log(binarySearch([2,4,3,5,4,3,2,6,],3));
+console.log(binarySearch([2,4,5,6,7,8],4));
 // Fixed the parentheses. 
